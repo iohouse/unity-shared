5fc66b3b99ff0568d035549c344434dee239d6ce
badscotsman <scott@iohouse.com>
2016-10-11 00:40:53 +0800

    Added touch input to spawn object

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,9 +1,10 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class Main : MonoBehaviour {
 
 	public KeyCode SpawnKey = KeyCode.Space;				// what key to press to spawn the object
+	public bool UseTouchOnMobile = true;					// whether to spawn object via touch input on mobile device platform
 	public GameObject ObjectToSpawn = null;					// the prefab of the object to spawn
 	public Transform SpawnPoint = null;						// the location to spawn the ObjectToSpawn at
 	public bool MoveObjectToTag = true;						// whether the spawned object will move towards the game object tagged Enemy
@@ -13,9 +14,30 @@ public class Main : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
-		// spawn object based on key press
-		if (Input.GetKeyDown(SpawnKey)) {
+		bool isSpawn = false;								// initialize the var we'll check for whether to spawn and object
 
+#if UNITY_ANDROID || UNITY_IOS
+
+		if (UseTouchOnMobile)
+		{
+			int fingerCount = 0;
+			foreach (Touch touch in Input.touches)
+			{
+				if (touch.phase != TouchPhase.Ended && touch.phase != TouchPhase.Canceled)
+					fingerCount++;
+			}
+			if (fingerCount > 0) isSpawn = true;
+		}
+
+#else		
+
+		// spawn object based on key press on standalone platforms
+		if (Input.GetKeyDown(SpawnKey)) isSpawn = true;
+
+#endif
+
+		if (isSpawn)
+		{
 			// spawn a prefab at the spawn point
 			if (ObjectToSpawn != null && SpawnPoint != null) {
 			
